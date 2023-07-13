9762e88488cf95c792552f64fd87b9cd42ce19fe
TeksMarques <taotaodoce@gmail.com>
2023-07-13 07:37:46 -0300

    feat: criação da classe Pessoa Eeitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,26 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * Class PessoaEleitora.
+ */
 
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+
+  /**
+   * Constructor.
+   */
+
+  public PessoaEleitora(String nome, String cpf) {
+    super.nome = nome;
+    this.setCpf(cpf);
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
