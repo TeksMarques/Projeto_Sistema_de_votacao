2a2006135e76f147f691284661c70e67fcac3fa6
TeksMarques <taotaodoce@gmail.com>
2023-07-13 07:32:23 -0300

    feat: criação da classe Pessoa Candidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,40 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * Classe Pessoa candidata.
+ */
 
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
+
+  /**
+   * Constructor.
+   */
+
+  public PessoaCandidata(String nome, int numero) {
+    super.nome = nome;
+    this.setNumero(numero);
+    this.setVotos(0);
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    votos += 1;
+  }
 }
