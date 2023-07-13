0d10e4e9a76b0256e941a30a3fcee4b244494a5a
TeksMarques <taotaodoce@gmail.com>
2023-07-13 07:16:53 -0300

    criação da classe Pessoa abstrata

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,16 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+/**
+ * Classe Pessoa abstrata.
+ */
+public abstract class Pessoa {
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
